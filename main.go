7da65eb2f3fb225ce5a843bbdced30d81df72835
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-05-28 20:11:52 +0100

    First Page scraper done

--- main.go
+++ main.go
@@ -10,13 +10,18 @@ func main() {
 	c := colly.NewCollector()
 
 	c.OnHTML(".market_listing_row_link", func(e *colly.HTMLElement) {
-		fmt.Println(e.Text)
-		//name := e.ChildText(".market_listing_row_link")
-		//fmt.Println(name)
+
+		price := e.ChildText("div.market_listing_price_listings_block > div.market_listing_right_cell.market_listing_their_price > span.market_table_value.normal_price > span.normal_price")
+		name := e.ChildText("div.market_listing_item_name_block > .market_listing_item_name")
+		fmt.Println(price, name)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnHTML(".pagebtn", func(e *colly.HTMLElement) {
+		c.Visit(e.Request.AbsoluteURL(e.Attr("href")))
 	})
 
 	c.Visit("https://steamcommunity.com/market/search?appid=730#p1_price_asc")

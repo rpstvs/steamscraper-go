ce2a5b0d9b8d5c7e39b908371ba68f4c3151ca72
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-06-13 16:28:37 +0100

    started search item

--- internals/handlers/getItem_handler.go
+++ internals/handlers/getItem_handler.go
@@ -1,11 +1,38 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+
+	"github.com/rpstvs/steamscraper-go/internals/database"
 )
 
-func HandlerGetPric(w http.ResponseWriter, r *http.Request) {
+type ApiConfig struct {
+	DB *database.Queries
+}
 
-	fmt.Println("cenas")
+func (cfg *ApiConfig) HandlerGetPric(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Skin string `json:"skin"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		fmt.Println("couldn't decode params")
+	}
+
+	id, err := cfg.DB.GetItemByName(r.Context(), params.Skin)
+
+	if err != nil {
+		fmt.Println("couldnt find the skin")
+	}
+
+	price := cfg.DB.
+
 }

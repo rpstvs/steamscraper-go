d485a74c079d9b7c2462d1a26a8845fc9a158c3d
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-07-05 18:23:44 +0100

    added cookie and jwt

--- internals/auth/jwt_auth.go
+++ internals/auth/jwt_auth.go
@@ -1,6 +1,7 @@
 package auth
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -9,6 +10,29 @@ import (
 func CreateToken(name string) string {
 	tokenSecret := os.Getenv("TOKEN_SECRET")
 
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{})
-	return ""
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Subject: name,
+	})
+
+	tokenString, err := token.SignedString([]byte(tokenSecret))
+
+	if err != nil {
+		fmt.Println("couldnt create token")
+		return ""
+	}
+
+	return tokenString
+}
+
+func ValidateToken(tokenString, tokenSecret string) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(tokenSecret), nil
+	})
+	if err != nil {
+		fmt.Println("couldnt parse the token")
+		return
+	}
+	if !token.Valid {
+		return
+	}
 }
--- internals/server/handler_login_steam.go
+++ internals/server/handler_login_steam.go
@@ -4,25 +4,18 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"time"
 
 	"github.com/google/uuid"
+	"github.com/rpstvs/steamscraper-go/internals/auth"
 	"github.com/rpstvs/steamscraper-go/internals/database"
 	"github.com/rpstvs/steamscraper-go/internals/steamapi"
 	"github.com/rpstvs/steamscraper-go/internals/utils"
-	"golang.org/x/oauth2"
 )
 
-var (
-	steamOAuthConfig = oauth2.Config{
-		ClientID:    "__-",
-		RedirectURL: "http://localhost:8080/auth/steam/callback",
-		Endpoint: oauth2.Endpoint{
-			AuthURL:  "https://steamcommunity.com/openid/login",
-			TokenURL: "https://steamcommunity.com/openid/login",
-		},
-		Scopes: []string{},
-	}
-)
+type User struct {
+	Name string `json:"name"`
+}
 
 func (cfg *Server) loginSteam(w http.ResponseWriter, r *http.Request) {
 	fullURL := "http://" + r.Host + r.RequestURI
@@ -43,10 +36,18 @@ func (cfg *Server) loginSteam(w http.ResponseWriter, r *http.Request) {
 		Steamid: id,
 	})
 
+	token := auth.CreateToken(id)
+
+	cookie := &http.Cookie{
+		Name:    name,
+		Value:   token,
+		Expires: time.Now().UTC().Add(24 * time.Hour),
+	}
+
+	http.SetCookie(w, cookie)
+
+	RespondWithJson(w, http.StatusOK, User{
+		Name: name,
+	})
+
 }
-
-//http://localhost:8080/v1/api/auth/steam?openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.mode=id_res&openid.op_endpoint=https%3A%2F%2Fsteamcommunity.com%2Fopenid%2Flogin&openid.claimed_id=https%3A%2F%2Fsteamcommunity.com%2Fopenid%2Fid%2F76561198000318676&openid.identity=https%3A%2F%2Fsteamcommunity.com%2Fopenid%2Fid%2F76561198000318676&openid.return_to=http%3A%2F%2Flocalhost%3A8080%2Fv1%2Fapi%2Fauth%2Fsteam&openid.response_nonce=2024-06-30T18%3A11%3A11ZqRt6FJABqlB%2BVowcPds03mcaDjk%3D&openid.assoc_handle=1234567890&openid.signed=signed%2Cop_endpoint%2Cclaimed_id%2Cidentity%2Creturn_to%2Cresponse_nonce%2Cassoc_handle&openid.sig=54%2FH3ptnfJutyx8LMXRHcE3qvwI%3D
-
-//Utilizador faz login
-// retirar o nome da steam
-// registar o user na DB

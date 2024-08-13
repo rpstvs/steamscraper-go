110049b027d76233b9372d9de0651ac0b343db0e
Rpstvs <125133153+rpstvs@users.noreply.github.com>
2024-08-13 07:41:17 +0100

    added monthly average function

--- internals/utils/price_change_calc.go
+++ internals/utils/price_change_calc.go
@@ -24,3 +24,21 @@ func WeeklyPriceChange(prices []float64) float64 {
 
 	return WeeklyChange
 }
+
+func MonthlyPrice(prices []float64) float64 {
+
+	var sum float64
+	var average float64
+	if len(prices) == 0 {
+		return 0.00
+	}
+	for x := range prices {
+		sum += prices[x]
+	}
+
+	average = sum / 30.0
+
+	monthlyChange := ((prices[0] - average) / average) * 100
+
+	return monthlyChange
+}
